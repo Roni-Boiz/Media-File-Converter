23f3e3ad6909aa00d212246288e2782622763144
Roni-Boiz <2019cs155@stu.ucsc.lk>
2024-09-06 04:37:21 +0530

    update SQL script

--- python/src/auth/init.sql
+++ python/src/auth/init.sql
@@ -1,15 +1,20 @@
-CREATE USER 'auth_user'@'%' IDENTIFIED BY 'authuser123';
+CREATE USER IF NOT EXISTS 'auth_user'@'%' IDENTIFIED BY 'authuser123';
 
-CREATE DATABASE auth;
+CREATE DATABASE IF NOT EXISTS auth;
 
 GRANT ALL PRIVILEGES ON auth.* TO 'auth_user'@'%';
 
 use auth;
 
-CREATE TABLE user (
+CREATE TABLE IF NOT EXISTS user (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL
 );
 
-INSERT INTO user (email, password) VALUES ('don@email.com', 'admin1234');
+INSERT INTO user (email, password)
+SELECT 'don@email.com', 'admin1234'
+FROM DUAL
+WHERE NOT EXISTS (
+    SELECT 1 FROM user WHERE email = 'don@email.com'
+);
